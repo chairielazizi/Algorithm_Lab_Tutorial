d79dc676d7c141181e452f9fe02d45cded58bec4
chairielazizi <azizihr2000@gmail.com>
2021-03-11 13:19:54 +0800

    add tutorial 1

---
+++ Tutorial 1/tuto1.py
@@ -0,0 +1,13 @@
+def insertionSort(a):
+    for j in range(1, len(a), 1):
+        key = a[j]
+        i = j - 1
+        while (i >= 0 and a[i] < key):
+            a[i + 1] = a[i]
+            i = i - 1
+        a[i + 1] = key
+    return a
+
+arr = [41, 51, 69, 36, 51, 68]
+data = insertionSort(arr)
+print(data)
